633ef767837d51104e93e5203f39e70ba2c981a0
Shimi Ulman <shimiuhlman@gmail.com>
2018-05-31 12:01:29 +0300

    new project

---
+++ app/src/main/java/app/nanit/com/happybirthdaynanit/HappyBirthdayEditInfoActivity.java
@@ -0,0 +1,13 @@
+package app.nanit.com.happybirthdaynanit;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HappyBirthdayEditInfoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_happy_birthday_edit_info);
+    }
+}
